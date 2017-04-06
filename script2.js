cec0baa8e12ba7c3674da39899d40eada3985530
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-04-06 00:52:42 -0500

    Added content to portfolio section. Also added media query styles.

--- script2.js
+++ script2.js
@@ -111,16 +111,23 @@ $(function() {
   // jQuery functions for interacting with portfolio page
 
   var divOpen1 = false;
+  var tech = 'Technologies';
+  var mov1 = '<video autoplay loop muted><source src="videos/cohort.mp4" type="video/mp4"></video>';
+  var tryit1 = '<a href="https://huevang-cohorttracker.herokuapp.com/">&#9734 Try it &#9734</a>';
+  var cohortTrackerP1 = 'Node.js | AngularJS | Express | PostgreSQL | jQuery';
+  var cohortTrackerP2 = 'username: Cloud | password: 1234';
   if (well) console.log('This is divOpen1:', divOpen1);
 
   $('#cohortTracker').on('click', function() {
     if (divOpen1) {
-      $('#cohortTracker .projectInfo').slideUp('normal', function() { $(this).remove(); });
+      $(this).removeClass('highlight');
+      $('#cohortTrackerDiv .projectInfo').slideUp('slow', function() { $(this).remove(); });
       if (well) console.log('This is divOpen1 if:', divOpen1);
       divOpen1 = false;
       if (well) console.log('This is divOpen1 if after change:', divOpen1);
     } else {
-      $('<div class=' + '"projectInfo"' + '></div>').appendTo('#cohortTracker').hide().slideDown();
+      $(this).addClass('highlight');
+      $('<div class=' + '"projectInfo"' + '>' + mov1 + '<h4>' + tech + '</h4><p>' + cohortTrackerP1 + '</p><h4>' + tryit1 + '</h4><p>' + cohortTrackerP2 + '</p></div>').appendTo('#cohortTrackerDiv').hide().slideDown('slow');
       if (well) console.log('This is divOpen1 else:', divOpen1);
       divOpen1 = true;
       if (well) console.log('This is divOpen1 else after change:', divOpen1);
@@ -128,16 +135,22 @@ $(function() {
   });
 
   var divOpen2 = false;
+  var mov2 = '<video autoplay loop muted><source src="videos/calculator.mp4" type="video/mp4"></video>';
+  var tryit2 = '<a href="https://huevang-calculator.herokuapp.com/">&#9734 Try it &#9734</a>';
+  var calculatorP1 = 'Node.js | Express | Bootstrap | jQuery';
+  var calculatorP2 = '';
   if (well) console.log('This is divOpen2:', divOpen2);
 
   $('#calculator').on('click', function() {
     if (divOpen2) {
-      $('#calculator .projectInfo').slideUp('normal', function() { $(this).remove(); });
+      $(this).removeClass('highlight');
+      $('#calculatorDiv .projectInfo').slideUp('slow', function() { $(this).remove(); });
       if (well) console.log('This is divOpen2 if:', divOpen2);
       divOpen2 = false;
       if (well) console.log('This is divOpen2 if after change:', divOpen2);
     } else {
-      $('<div class=' + '"projectInfo"' + '></div>').appendTo('#calculator').hide().slideDown();
+      $(this).addClass('highlight');
+      $('<div class=' + '"projectInfo"' + '>' + mov2 + '<h4>' + tech + '</h4><p>' + calculatorP1 + '</p><h4>' + tryit2 + '</h4><p>' + calculatorP2 + '</p></div>').appendTo('#calculatorDiv').hide().slideDown('slow');
       if (well) console.log('This is divOpen2 else:', divOpen2);
       divOpen2 = true;
       if (well) console.log('This is divOpen2 else after change:', divOpen2);
@@ -145,16 +158,22 @@ $(function() {
   });
 
   var divOpen3 = false;
+  var mov3 = '<video autoplay loop muted><source src="videos/etch.mp4" type="video/mp4"></video>';
+  var tryit3 = '<a href="#">&#9734 Try it &#9734</a>';
+  var etchASketchP1 = 'HTML5 | CSS3 | jQuery';
+  var etchASketchP2 = '';
   if (well) console.log('This is divOpen3:', divOpen3);
 
   $('#etchASketch').on('click', function() {
     if (divOpen3) {
-      $('#etchASketch .projectInfo').slideUp('normal', function() { $(this).remove(); });
+      $(this).removeClass('highlight');
+      $('#etchASketchDiv .projectInfo').slideUp('slow', function() { $(this).remove(); });
       if (well) console.log('This is divOpen3 if:', divOpen3);
       divOpen3 = false;
       if (well) console.log('This is divOpen3 if after change:', divOpen3);
     } else {
-      $('<div class=' + '"projectInfo"' + '></div>').appendTo('#etchASketch').hide().slideDown();
+      $(this).addClass('highlight');
+      $('<div class=' + '"projectInfo"' + '>' + mov3 + '<h4>' + tech + '</h4><p>' + etchASketchP1 + '</p><h4>' + tryit3 + '</h4><p>' + etchASketchP2 + '</p></div>').appendTo('#etchASketchDiv').hide().slideDown('slow');
       if (well) console.log('This is divOpen3 else:', divOpen3);
       divOpen3 = true;
       if (well) console.log('This is divOpen3 else after change:', divOpen3);
@@ -162,16 +181,22 @@ $(function() {
   });
 
   var divOpen4 = false;
+  var mov4 = '<video autoplay loop muted><source src="videos/google.mp4" type="video/mp4"></video>';
+  var tryit4 = '<a href="#">&#9734 Try it &#9734</a>';
+  var googleP1 = 'HTML5 | CSS3';
+  var googleP2 = '';
   if (well) console.log('This is divOpen4:', divOpen4);
 
   $('#google').on('click', function() {
     if (divOpen4) {
-      $('#google .projectInfo').slideUp('normal', function() { $(this).remove(); });
+      $(this).removeClass('highlight');
+      $('#googleDiv .projectInfo').slideUp('slow', function() { $(this).remove(); });
       if (well) console.log('This is divOpen4 if:', divOpen4);
       divOpen4 = false;
       if (well) console.log('This is divOpen4 if after change:', divOpen4);
     } else {
-      $('<div class=' + '"projectInfo"' + '></div>').appendTo('#google').hide().slideDown();
+      $(this).addClass('highlight');
+      $('<div class=' + '"projectInfo"' + '>' + mov4 + '<h4>' + tech + '</h4><p>' + googleP1 + '</p><h4>' + tryit4 + '</h4><p>' + googleP2 + '</p></div>').appendTo('#googleDiv').hide().slideDown('slow');
       if (well) console.log('This is divOpen4 else:', divOpen4);
       divOpen4 = true;
       if (well) console.log('This is divOpen4 else after change:', divOpen4);
@@ -179,16 +204,22 @@ $(function() {
   });
 
   var divOpen5 = false;
+  var mov5 = '<video autoplay loop muted><source src="videos/poker.mp4" type="video/mp4"></video>';
+  var tryit5 = '<a class="notavailable" href="#">&#9734 Try it &#9734</a>';
+  var pokerAppP1 = 'Node.js | Express | AngularJS | PostgreSQL | Bootstrap | Socket.io';
+  var pokerAppP2 = '';
   if (well) console.log('This is divOpen5:', divOpen5);
 
   $('#pokerApp').on('click', function() {
     if (divOpen5) {
-      $('#pokerApp .projectInfo').slideUp('normal', function() { $(this).remove(); });
+      $(this).removeClass('highlight');
+      $('#pokerAppDiv .projectInfo').slideUp('slow', function() { $(this).remove(); });
       if (well) console.log('This is divOpen5 if:', divOpen5);
       divOpen5 = false;
       if (well) console.log('This is divOpen5 if after change:', divOpen5);
     } else {
-      $('<div class=' + '"projectInfo"' + '></div>').appendTo('#pokerApp').hide().slideDown();
+      $(this).addClass('highlight');
+      $('<div class=' + '"projectInfo"' + '>' + mov5 + '<h4>' + tech + '</h4><p>' + pokerAppP1 + '</p><h4>' + tryit5 + '</h4><p>' + pokerAppP2 + '</p></div>').appendTo('#pokerAppDiv').hide().slideDown('slow');
       if (well) console.log('This is divOpen5 else:', divOpen5);
       divOpen5 = true;
       if (well) console.log('This is divOpen5 else after change:', divOpen5);
